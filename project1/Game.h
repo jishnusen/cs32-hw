641142d5678827bb9454de8a6d304a0cdea266c2
jishnusen <jishnu1@gmail.com>
2021-01-08 18:42:28 -0800

    Draft 1 of project 1

---
+++ project1/Game.h
@@ -0,0 +1,20 @@
+#ifndef GAME_H_
+#define GAME_H_
+
+#include "globals.h"
+
+class Game
+{
+  public:
+        // Constructor/destructor
+    Game(int rows, int cols, int nFlatulans);
+    ~Game();
+
+        // Mutators
+    void play();
+
+  private:
+    City* m_city;
+};
+
+#endif
