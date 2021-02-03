f542ba9f986bb90abab9362fab41b54ed959995d
jishnusen <jishnu1@gmail.com>
2021-02-02 20:30:26 -0800

    remove main function

--- homework2/mazestack.cpp
+++ homework2/mazestack.cpp
@@ -51,14 +51,3 @@ bool pathExists(string maze[], int nRows, int nCols, int sr, int sc, int er,
 
   return false;
 }
-
-int main() {
-  string maze[10] = {"XXXXXXXXXX", "X........X", "XX.X.XXXXX", "X..X.X...X",
-                     "X..X...X.X", "XXXX.XXX.X", "X.X....XXX", "X..XX.XX.X",
-                     "X...X....X", "XXXXXXXXXX"};
-
-  if (pathExists(maze, 10, 10, 6, 4, 1, 1))
-    cout << "Solvable!" << endl;
-  else
-    cout << "Out of luck!" << endl;
-}
