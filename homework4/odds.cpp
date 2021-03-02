a2a6af51ec5e89ecd802459d21d32fe50accff42
jishnusen <jishnu1@gmail.com>
2021-03-01 16:50:47 -0800

    Problem 3

---
+++ homework4/odds.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Remove the odd integers from v.
+// It is acceptable if the order of the remaining even integers is not
+// the same as in the original vector.
+void removeOdds(vector<int>& v) {
+  for (auto it = v.begin(); it != v.end();) {
+    if (*it % 2) {
+      it = v.erase(it);
+    } else
+      it++;
+  }
+}
+
+void test() {
+  int a[8] = {2, 8, 5, 6, 7, 3, 4, 1};
+  vector<int> x(a, a + 8);  // construct x from the array
+  assert(x.size() == 8 && x.front() == 2 && x.back() == 1);
+  removeOdds(x);
+  assert(x.size() == 4);
+  sort(x.begin(), x.end());
+  int expect[4] = {2, 4, 6, 8};
+  for (int k = 0; k < 4; k++) assert(x[k] == expect[k]);
+}
+
+int main() {
+  test();
+  cout << "Passed" << endl;
+}
